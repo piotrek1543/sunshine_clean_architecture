a48443bd5c1c1b252a21e5020b3dcf027aa49667
Piotr <piotrekert90@gmail.com>
2019-02-13 20:01:52 +0100

    Update BrowseAdapter layout and revert strings changes.

--- mobile-ui/src/main/java/com/piotrek1543/android/boilerplate/ui/browse/BrowseAdapter.kt
+++ mobile-ui/src/main/java/com/piotrek1543/android/boilerplate/ui/browse/BrowseAdapter.kt
@@ -4,7 +4,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.piotrek1543.android.boilerplate.ui.R
 import com.piotrek1543.android.boilerplate.ui.model.ForecastViewModel
 import javax.inject.Inject
@@ -15,19 +18,21 @@ class BrowseAdapter @Inject constructor() : RecyclerView.Adapter<BrowseAdapter.V
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val forecast = forecastList[position]
-        holder.nameText.text = forecast.date
-        holder.titleText.text = forecast.description
+        holder.dateTV.text = forecast.date
+        holder.descriptionTV.text = forecast.description
+        holder.tempMaxTV.text = forecast.tempMax
+        holder.tempMinTV.text = forecast.tempMin
 
-        /*Glide.with(holder.itemView.context)
-                .load(bufferoo.avatar)
+        Glide.with(holder.itemView.context)
+                .load(R.mipmap.ic_launcher)
                 .apply(RequestOptions.circleCropTransform())
-                .into(holder.avatarImage)*/
+                .into(holder.weatherIconIV)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemView = LayoutInflater
                 .from(parent.context)
-                .inflate(R.layout.item_bufferoo, parent, false)
+                .inflate(R.layout.forecast_list_item, parent, false)
         return ViewHolder(itemView)
     }
 
@@ -36,8 +41,11 @@ class BrowseAdapter @Inject constructor() : RecyclerView.Adapter<BrowseAdapter.V
     }
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var nameText: TextView = view.findViewById(R.id.text_name)
-        var titleText: TextView = view.findViewById(R.id.text_title)
+        val weatherIconIV : ImageView = view.findViewById(R.id.weather_icon)
+        val dateTV: TextView = view.findViewById(R.id.date)
+        val descriptionTV: TextView = view.findViewById(R.id.weather_description)
+        val tempMaxTV: TextView = view.findViewById(R.id.high_temperature)
+        val tempMinTV: TextView = view.findViewById(R.id.low_temperature)
     }
 
 }
