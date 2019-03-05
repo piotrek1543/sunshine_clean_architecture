bad58f39a26ddea1e38a440baf0497463dce294a
Piotr <piotrekert90@gmail.com>
2019-03-05 20:32:28 +0100

    Convert SunshineDateUtils from Java to Kotlin.

--- mobile-ui/src/main/java/com/piotrek1543/android/boilerplate/ui/utils/SunshineDateUtils.kt
+++ mobile-ui/src/main/java/com/piotrek1543/android/boilerplate/ui/utils/SunshineDateUtils.kt
@@ -13,25 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.piotrek1543.android.boilerplate.ui.utils;
+package com.piotrek1543.android.boilerplate.ui.utils
 
-import android.content.Context;
-import android.text.format.DateUtils;
-
-import com.piotrek1543.android.boilerplate.ui.R;
-
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-import java.util.TimeZone;
-import java.util.concurrent.TimeUnit;
+import android.content.Context
+import android.text.format.DateUtils
+import com.piotrek1543.android.boilerplate.ui.R
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 /**
  * Class for handling date conversions that are useful for Sunshine.
  */
-public final class SunshineDateUtils {
-
-    /* Milliseconds in a day */
-    public static final long DAY_IN_MILLIS = TimeUnit.DAYS.toMillis(1);
+object SunshineDateUtils {
 
     /**
      * This method returns the number of milliseconds (UTC time) for today's date at midnight in
@@ -40,15 +34,18 @@ public final class SunshineDateUtils {
      * Epoch time converter, you may be confused that it tells you this time stamp represents 8:00
      * PM on September 19th local time, rather than September 20th. We're concerned with the GMT
      * date here though, which is correct, stating September 20th, 2016 at midnight.
-     * <p>
+     *
+     *
      * As another example, if you are in Hong Kong and the day is September 20th, 2016 and it is
      * 6:30 PM, this method will return 1474329600000. Again, if you plug this number into an Epoch
      * time converter, you won't get midnight for your local time zone. Just keep in mind that we
      * are just looking at the GMT date here.
-     * <p>
+     *
+     *
      * This method will ALWAYS return the date at midnight (in GMT time) for the time zone you
      * are currently in. In other words, the GMT date will always represent your date.
-     * <p>
+     *
+     *
      * Since UTC / GMT time are the standard for all time zones in the world, we use it to
      * normalize our dates that are stored in the database. When we extract values from the
      * database, we adjust for the current time zone using time zone offsets.
@@ -56,46 +53,35 @@ public final class SunshineDateUtils {
      * @return The number of milliseconds (UTC / GMT) for today's date at midnight in the local
      * time zone
      */
-    public static long getNormalizedUtcDateForToday() {
-
-        /*
+    /*
          * This number represents the number of milliseconds that have elapsed since January
          * 1st, 1970 at midnight in the GMT time zone.
-         */
-        long utcNowMillis = System.currentTimeMillis();
-
-        /*
+         *//*
          * This TimeZone represents the device's current time zone. It provides us with a means
          * of acquiring the offset for local time from a UTC time stamp.
-         */
-        TimeZone currentTimeZone = TimeZone.getDefault();
-
-        /*
+         *//*
          * The getOffset method returns the number of milliseconds to add to UTC time to get the
          * elapsed time since the epoch for our current time zone. We pass the current UTC time
          * into this method so it can determine changes to account for daylight savings time.
-         */
-        long gmtOffsetMillis = currentTimeZone.getOffset(utcNowMillis);
-
-        /*
+         *//*
          * UTC time is measured in milliseconds from January 1, 1970 at midnight from the GMT
          * time zone. Depending on your time zone, the time since January 1, 1970 at midnight (GMT)
          * will be greater or smaller. This variable represents the number of milliseconds since
          * January 1, 1970 (GMT) time.
-         */
-        long timeSinceEpochLocalTimeMillis = utcNowMillis + gmtOffsetMillis;
-
-        /* This method simply converts milliseconds to days, disregarding any fractional days */
-        long daysSinceEpochLocal = TimeUnit.MILLISECONDS.toDays(timeSinceEpochLocalTimeMillis);
-
-        /*
+         *//* This method simply converts milliseconds to days, disregarding any fractional days *//*
          * Finally, we convert back to milliseconds. This time stamp represents today's date at
          * midnight in GMT time. We will need to account for local time zone offsets when
          * extracting this information from the database.
-         */
+         */ val normalizedUtcDateForToday: Long
+        get() {
+            val utcNowMillis = System.currentTimeMillis()
+            val currentTimeZone = TimeZone.getDefault()
+            val gmtOffsetMillis = currentTimeZone.getOffset(utcNowMillis).toLong()
+            val timeSinceEpochLocalTimeMillis = utcNowMillis + gmtOffsetMillis
+            val daysSinceEpochLocal = TimeUnit.MILLISECONDS.toDays(timeSinceEpochLocalTimeMillis)
 
-        return TimeUnit.DAYS.toMillis(daysSinceEpochLocal);
-    }
+            return TimeUnit.DAYS.toMillis(daysSinceEpochLocal)
+        }
 
     /**
      * This method returns the number of days since the epoch (January 01, 1970, 12:00 Midnight UTC)
@@ -104,14 +90,15 @@ public final class SunshineDateUtils {
      * @param utcDate A date in milliseconds in UTC time.
      * @return The number of days from the epoch to the date argument.
      */
-    private static long elapsedDaysSinceEpoch(long utcDate) {
-        return TimeUnit.MILLISECONDS.toDays(utcDate);
+    private fun elapsedDaysSinceEpoch(utcDate: Long): Long {
+        return TimeUnit.MILLISECONDS.toDays(utcDate)
     }
 
     /**
      * Helper method to convert the database representation of the date into something to display
      * to users. As classy and polished a user experience as "1474061664" is, we can do better.
-     * <p/>
+     *
+     *
      * The day string for forecast uses the following logic:
      * For today: "Today, June 8"
      * For tomorrow:  "Tomorrow
@@ -123,7 +110,7 @@ public final class SunshineDateUtils {
      * @return A user-friendly representation of the date such as "Today, June 8", "Tomorrow",
      * or "Friday"
      */
-    public static String getFriendlyDateString(Context context, long date, int position) {
+    fun getFriendlyDateString(context: Context, date: Long, position: Int): String {
 
         /*
          * NOTE: localDate should be localDateMidnightMillis and should be straight from the
@@ -139,19 +126,19 @@ public final class SunshineDateUtils {
          * to compare the number of days that have passed since the epoch (January 1, 1970 at
          * 00:00 GMT)
          */
-        long daysFromEpochToProvidedDate = elapsedDaysSinceEpoch(date);
+        val daysFromEpochToProvidedDate = elapsedDaysSinceEpoch(date)
 
         /*
          * As a basis for comparison, we use the number of days that have passed from the epoch
          * until today.
          */
-        long daysFromEpochToToday = elapsedDaysSinceEpoch(System.currentTimeMillis());
-        String dayName = getDayName(context, date);
-        String localizedDayName = new SimpleDateFormat("EEEE", Locale.ENGLISH).format(date);
-        int flags = DateUtils.FORMAT_NO_YEAR
-                | DateUtils.FORMAT_SHOW_WEEKDAY
-                | DateUtils.FORMAT_SHOW_TIME;
-        String readableDate = getReadableDateString(context, date, flags);
+        val daysFromEpochToToday = elapsedDaysSinceEpoch(System.currentTimeMillis())
+        val dayName = getDayName(context, date)
+        val localizedDayName = SimpleDateFormat("EEEE", Locale.ENGLISH).format(date)
+        var flags = (DateUtils.FORMAT_NO_YEAR
+                or DateUtils.FORMAT_SHOW_WEEKDAY
+                or DateUtils.FORMAT_SHOW_TIME)
+        var readableDate = getReadableDateString(context, date, flags)
 
         if (daysFromEpochToProvidedDate == daysFromEpochToToday) {
             /*
@@ -171,29 +158,29 @@ public final class SunshineDateUtils {
                  * https://developer.android.com/reference/android/text/format/DateFormat.html#getBestDateTimePattern
                  */
                 if (position == 0)
-                    flags = DateUtils.FORMAT_SHOW_DATE
-                            | DateUtils.FORMAT_NO_YEAR
-                            | DateUtils.FORMAT_SHOW_WEEKDAY
-                            | DateUtils.FORMAT_SHOW_TIME;
+                    flags = (DateUtils.FORMAT_SHOW_DATE
+                            or DateUtils.FORMAT_NO_YEAR
+                            or DateUtils.FORMAT_SHOW_WEEKDAY
+                            or DateUtils.FORMAT_SHOW_TIME)
 
-                readableDate = getReadableDateString(context, date, flags);
-                return readableDate.replace(localizedDayName, dayName);
+                readableDate = getReadableDateString(context, date, flags)
+                return readableDate.replace(localizedDayName, dayName)
             } else {
 
 
-                return readableDate;
+                return readableDate
             }
         } else if (daysFromEpochToProvidedDate < daysFromEpochToToday + 2) {
             /* If the input date is less than a week in the future, just return the day name. */
-            return readableDate.replace(localizedDayName, dayName);
+            return readableDate.replace(localizedDayName, dayName)
         } else {
-            flags = DateUtils.FORMAT_SHOW_DATE
-                    | DateUtils.FORMAT_ABBREV_WEEKDAY
-                    | DateUtils.FORMAT_NO_YEAR
-                    | DateUtils.FORMAT_SHOW_WEEKDAY
-                    | DateUtils.FORMAT_SHOW_TIME;
+            flags = (DateUtils.FORMAT_SHOW_DATE
+                    or DateUtils.FORMAT_ABBREV_WEEKDAY
+                    or DateUtils.FORMAT_NO_YEAR
+                    or DateUtils.FORMAT_SHOW_WEEKDAY
+                    or DateUtils.FORMAT_SHOW_TIME)
 
-            return DateUtils.formatDateTime(context, date, flags);
+            return DateUtils.formatDateTime(context, date, flags)
         }
     }
 
@@ -205,8 +192,8 @@ public final class SunshineDateUtils {
      * @param timeInMillis Time in milliseconds since the epoch (local time)
      * @return The formatted date string
      */
-    private static String getReadableDateString(Context context, long timeInMillis, int flags) {
-        return DateUtils.formatDateTime(context, timeInMillis, flags);
+    private fun getReadableDateString(context: Context, timeInMillis: Long, flags: Int): String {
+        return DateUtils.formatDateTime(context, timeInMillis, flags)
     }
 
     /**
@@ -217,25 +204,22 @@ public final class SunshineDateUtils {
      * @param dateInMillis The date in milliseconds (UTC time)
      * @return the string day of the week
      */
-    private static String getDayName(Context context, long dateInMillis) {
+    private fun getDayName(context: Context, dateInMillis: Long): String {
         /*
          * If the date is today, return the localized version of "Today" instead of the actual
          * day name.
          */
-        long daysFromEpochToProvidedDate = elapsedDaysSinceEpoch(dateInMillis);
-        long daysFromEpochToToday = elapsedDaysSinceEpoch(System.currentTimeMillis());
+        val daysFromEpochToProvidedDate = elapsedDaysSinceEpoch(dateInMillis)
+        val daysFromEpochToToday = elapsedDaysSinceEpoch(System.currentTimeMillis())
 
-        int daysAfterToday = (int) (daysFromEpochToProvidedDate - daysFromEpochToToday);
+        return when ((daysFromEpochToProvidedDate - daysFromEpochToToday).toInt()) {
+            0 -> context.getString(R.string.today)
+            1 -> context.getString(R.string.tomorrow)
 
-        switch (daysAfterToday) {
-            case 0:
-                return context.getString(R.string.today);
-            case 1:
-                return context.getString(R.string.tomorrow);
-
-            default:
-                SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
-                return dayFormat.format(dateInMillis);
+            else -> {
+                val dayFormat = SimpleDateFormat("EEEE", Locale.ENGLISH)
+                dayFormat.format(dateInMillis)
+            }
         }
     }
 }
