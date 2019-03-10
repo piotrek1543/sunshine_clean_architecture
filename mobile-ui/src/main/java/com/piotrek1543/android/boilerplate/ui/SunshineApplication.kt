34459ddd22090b2780d4233a1d849f2b4172320f
Piotr Ekert <piotrekert90@gmail.com>
2019-03-10 18:46:26 +0100

    Update SunshineApplication - LeakCanary, StrictMode and Timber.

--- mobile-ui/src/main/java/com/piotrek1543/android/boilerplate/ui/SunshineApplication.kt
+++ mobile-ui/src/main/java/com/piotrek1543/android/boilerplate/ui/SunshineApplication.kt
@@ -1,6 +1,9 @@
 package com.piotrek1543.android.boilerplate.ui
 
+import android.os.StrictMode
 import com.piotrek1543.android.boilerplate.ui.injection.DaggerApplicationComponent
+import com.piotrek1543.android.boilerplate.ui.reporting.ReleaseTree
+import com.squareup.leakcanary.LeakCanary
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import timber.log.Timber
@@ -14,6 +17,31 @@ class SunshineApplication : DaggerApplication() {
     override fun onCreate() {
         super.onCreate()
         setupTimber()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                    StrictMode.ThreadPolicy.Builder()
+                            .detectAll()
+                            .penaltyFlashScreen()
+                            .penaltyLog()
+                            .build()
+            )
+            StrictMode.setVmPolicy(
+                    StrictMode.VmPolicy.Builder()
+                            .detectAll()
+                            .penaltyLog()
+                            .build()
+            )
+
+            LeakCanary.install(this)
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(ReleaseTree())
+        }
     }
 
     private fun setupTimber() {
