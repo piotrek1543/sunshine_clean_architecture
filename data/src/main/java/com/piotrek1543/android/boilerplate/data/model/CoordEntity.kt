41889a96b9621a63400d1c9cab0830cd256390ef
Piotr Ekert <piotrekert90@gmail.com>
2019-03-17 20:31:29 +0100

    Fix latest commit - add missing release config file.

--- data/src/main/java/com/piotrek1543/android/boilerplate/data/model/CoordEntity.kt
+++ data/src/main/java/com/piotrek1543/android/boilerplate/data/model/CoordEntity.kt
@@ -5,7 +5,7 @@ package com.piotrek1543.android.boilerplate.data.model
  * Representation for a [CoordEntity] fetched from an external layer data source
  */
 data class CoordEntity(
-        val cityId: Long? = null,
+        val cityId: Int? = null,
         val lon: Double? = null,
         val lat: Double? = null
 )
